55e57c9e754625dfc5018914599a7a92f2cc1f18
Hemlock4414 <antonio.marrara@gmx.ch>
2026-08-04 11:12:12 +0200

    feat: adding coin code

--- models/coin.class.js
+++ models/coin.class.js
@@ -1,23 +1,32 @@
 class Coin extends CollidableObject {
-    height = 80;
-    width = 80;
+
+    height = 120;
+
+    width = 120;
+
     IMAGES = [
         'img/8_coin/coin_1.png',
         'img/8_coin/coin_2.png'
     ];
 
-    constructor() {
+    constructor(index = 0) {
         super().loadImage(this.IMAGES[0]);
+
         this.loadImages(this.IMAGES);
-        this.x = 200 + Math.random() * 2000; // verteilt über das gesamte Level
-        this.y = 50 + Math.random() * 100;   // schwebt in der Luft (oberhalb von groundY = 160)
+
+        // Größerer, garantierter Mindestabstand statt rein zufälliger Verteilung über das ganze Level
+        this.x = 200 + index * 250 + Math.random() * 100;
+
+        this.y = 90 + Math.random() * 100; // schwebt niedriger als zuvor, weiterhin in der Luft
+
         this.animate();
     }
 
+    // Langsames Pulsieren zwischen den beiden Bildern (kein Gehen wie bei Chicken, daher eigene, einfache Animation statt playAnimation())
     animate() {
         setInterval(() => {
             this.currentImage = (this.currentImage + 1) % this.IMAGES.length;
             this.img = this.imageCache[this.IMAGES[this.currentImage]];
-        }, 400); // langsames Pulsieren zwischen den beiden Bildern
+        }, 700);
     }
 }
